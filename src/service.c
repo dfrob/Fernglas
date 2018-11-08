6f2838ffbd2d806c5b9a3a3b4c29609a5318cd6c
Dirk <mail@d-froboese.de>
2018-11-08 16:39:04 +0100

    Serviceschnittstelle begonnen zu implementieren.

--- src/service.c
+++ src/service.c
@@ -1,21 +1,12 @@
 /*----------------------------------------------------------------------*/
-/* FAT file system sample project for FatFs            (C)ChaN, 2018    */
+/* Service Schnittstelle                                                */
 /*----------------------------------------------------------------------*/
 
 #include <string.h>
-//#include "STM32F100.h"
-//#include "uart_stm32f1.h"
 #include "service.h"
 #include "stm32f411xe.h"
 #include "xprintf.h"
 #include "cmsis_os.h"
-
-//typedef unsigned int	UINT;	/* int must be 16-bit or 32-bit */
-//typedef unsigned char	BYTE;	/* char must be 8-bit */
-//typedef uint16_t		WORD;	/* 16-bit unsigned integer */
-//typedef uint16_t		WCHAR;	/* 16-bit unsigned integer */
-//typedef uint32_t		DWORD;	/* 32-bit unsigned integer */
-//typedef uint64_t		QWORD;	/* 64-bit unsigned integer */
 
 
 uint32_t AccSize;				/* Work register for fs command */
@@ -26,43 +17,17 @@ unsigned char Buff[4096] __attribute__ ((aligned (4))) ;	/* Working buffer */
 
 static
 const char HelpMsg[] =
-	"[Disk contorls]\n"
-	" di <pd#> - Initialize disk\n"
-	" dd [<pd#> <lba>] - Dump a secrtor\n"
-	" ds <pd#> - Show disk status\n"
-	"[Buffer controls]\n"
-	" bd <ofs> - Dump working buffer\n"
-	" be <ofs> [<data>] ... - Edit working buffer\n"
-	" br <pd#> <lba> [<count>] - Read disk into working buffer\n"
-	" bw <pd#> <lba> [<count>] - Write working buffer into disk\n"
-	" bf <val> - Fill working buffer\n"
-	"[File system controls]\n"
-	" fi <ld#> [<mount>]- Force initialized the volume\n"
-	" fs [<path>] - Show volume status\n"
-	" fl [<path>] - Show a directory\n"
-	" fo <mode> <file> - Open a file\n"
-	" fc - Close the file\n"
-	" fe <ofs> - Move fp in normal seek\n"
-	" fd <len> - Read and dump the file\n"
-	" fr <len> - Read the file\n"
-	" fw <len> <val> - Write to the file\n"
-	" fn <org.name> <new.name> - Rename an object\n"
-	" fu <name> - Unlink an object\n"
-	" fv - Truncate the file at current fp\n"
-	" fk <name> - Create a directory\n"
-	" fa <atrr> <mask> <object name> - Change attribute of an object\n"
-	" ft <year> <month> <day> <hour> <min> <sec> <name> - Change timestamp of an object\n"
-	" fx <src.file> <dst.file> - Copy a file\n"
-	" fg <path> - Change current directory\n"
-	" fq - Show current directory\n"
-	" fb <name> - Set volume label\n"
-	" fm <ld#> <type> <csize> - Create file system\n"
-	" fz [<len>] - Change/Show R/W length for fr/fw/fx command\n"
-	"[Misc commands]\n"
-	" md[b|h|w] <addr> [<count>] - Dump memory\n"
-	" mf <addr> <value> <count> - Fill memory\n"
-	" me[b|h|w] <addr> [<value> ...] - Edit memory\n"
-	" t [<year> <mon> <mday> <hour> <min> <sec>] - Set/Show RTC\n"
+  "/n*-HELP------COMMAND OVERVIEW----------------------------------------*\n"
+	"| cmd     Command   Params     Description                          |\n"
+	"| ---     -------   ------     -----------                          |\n"
+	"| H       help      -          print help menu                      |\n"
+	"| I       info      -          print device information             |\n"
+	"| D       display   -          control display                      |\n"
+	"| P       pwm       chan, val  pwm control                          |\n"
+	"| Q       Quit      -          exit help menu                       |\n"
+	"| S       status    kat        state of different device parameters |\n"
+	"| T       tranf. r. t          transfer rate of comm. data          |\n"
+	"*-------------------------------------------------------------------*\n"
 	"\n";
 
 
@@ -71,7 +36,7 @@ void ServiceTask (void)
 {
 	char *ptr;
 
-	xputs("Service Task gestartet\n");
+	xputs("Service Task started\n");
 
 	for (;;) {
 		xputc('>');
@@ -82,10 +47,18 @@ void ServiceTask (void)
 //		xprintf("Str:%s ", Line);
 
 		switch (*ptr++) {
-		case '?' :	/* Show Command List */
+		case 'h' :	/* print help menu */
+		case 'H' :
 			xputs(HelpMsg);
 			break;
-		case 'm' :	/* Memory dump/fill/edit */
+		case 'i' :	/* print device information */
+		case 'I' :
+			break;
+		case 'd' :	/* control display */
+		case 'D' :
+			break;
+		case 'p' :	/* pwm control */
+		case 'P' :
 			switch (*ptr++) {
 			case 'd' :
 				switch (*ptr++) {
@@ -95,7 +68,7 @@ void ServiceTask (void)
 					break;
 				default:
 					break;
-				} // switch 3. Zeichen
+				} // switch 3. character
 				break;
 			case 'f' :
 				break;
@@ -107,12 +80,12 @@ void ServiceTask (void)
 					break;
 				}
 				break;
-			} // switch 2. Zeichen
+			} // switch 2. character
 			break;
 		default:
-			xprintf("\n%s -> keine gültige Eingabe\n", Line);
+			xprintf("\n%s -> invalid input\n", Line);
 			break;
-		} // switch 1. Zeichen
+		} // switch 1. character
 	}
 }
 
